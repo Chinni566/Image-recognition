e1900d0869239bded70b4ac2b1508809fa762bfa
Neeraj <78613839+Chinni566@users.noreply.github.com>
2021-12-14 17:07:25 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,39 +1,38 @@
 Webcam.set({
-    width:350,
-    height:300,
-    image_format:'png',
-    png_quality:90
+    width: 350,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90
 })
 
-camera=document.getElementById("camera");
+camera = document.getElementById("camera");
 Webcam.attach(camera);
 
-function take_snapshot(){
-Webcam.snap(function(photo){
-    document.getElementById("result").innerHTML="<img id='Captured_image' src='"+photo+"'>"
-})
+function snapshot() {
+    Webcam.snap(function (photo) {
+        document.getElementById("result").innerHTML = "<img id='CapturedImage' src='" + photo + "'>"
+    })
 }
 
 console.log(ml5.version);
-classifer=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LWI67u14D/model.json',modelLoaded);
+classifer = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/LWI67u14D/model.json', modelLoaded);
 
-function modelLoaded(){
-console.log("model loaded");
- }
-
-function check(){
- img=document.getElementById("Captured_image");
- classifer.classify(image,GotResults);
+function modelLoaded() {
+    console.log("model loaded");
 }
 
-function GotResults(error,results){
-if(error){
-    console.log(error) ;
+function check() {
+    img = document.getElementById("CapturedImage");
+    classifer.classify(img, Results);
 }
-else{
-    console.log(results);
-    document.getElementById("objectname").innerHTML=results[0].label;
-    document.getElementById("objectaccuracy").innerHTML=results[0].confidence.toFixed(3);
 
-}
+function Results(error, results) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        document.getElementById("objectname").innerHTML = results[0].label;
+        document.getElementById("Accuracy").innerHTML = results[0].confidence.toFixed(3);
+
+    }
 }
